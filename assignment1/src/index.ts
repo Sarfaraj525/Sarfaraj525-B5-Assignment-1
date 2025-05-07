fa94f51a48f051a8e2639697680417d179d8b4ed
Sarfaraj Nawaz Chowdhury <sarfarajeee525@gmail.com>
2025-05-07 21:55:10 +0600

    first commit

---
+++ assignment1/src/index.ts
@@ -0,0 +1,6 @@
+function formatString(input: string, toUpper: boolean = true): string {
+  return toUpper ? input.toUpperCase() : input.toLowerCase();
+}
+
+console.log(formatString("Hello"));
+console.log(formatString("Hello", false));
